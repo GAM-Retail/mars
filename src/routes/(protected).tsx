16d2316c2d52772e690ba7ff8d6788e4e2264a31
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-04-07 14:49:07 +0700

    feat(protected): redirect unauthenticated users to login page
    Add navigation effect to redirect when user data is absent and guard rendering
    of children with a condition to prevent unauthorized access.

--- src/routes/(protected).tsx
+++ src/routes/(protected).tsx
@@ -1,21 +1,23 @@
-import {createAsync, type RouteDefinition, RouteSectionProps} from "@solidjs/router";
-import {getUser} from "~/lib";
-import CurrentUserProvider from "~/components/CurrentUserProvider";
-
-export const route = {
-  preload() {
-    getUser();
-  }
-} satisfies RouteDefinition;
+import { createAsync, RouteSectionProps, useNavigate } from '@solidjs/router';
+import { getUser } from '~/lib';
+import CurrentUserProvider from '~/components/CurrentUserProvider';
+import { createEffect, Show } from 'solid-js';
 
 export default function Protected(props: Readonly<RouteSectionProps>) {
   const user = createAsync(() => getUser(), { deferStream: true });
 
+  const navigate = useNavigate();
+  createEffect(() => {
+    if (!user()) {
+      navigate('/login', { replace: true });
+    }
+  });
   return (
     <main class="w-full">
-      <CurrentUserProvider currentUser={user()}>
-        {props.children}
-      </CurrentUserProvider>
+      <Show when={user()}>
+        {/* @ts-expect-error null and/or undefined check is already done in the createEffect and Show Component */}
+        <CurrentUserProvider currentUser={user()}>{props.children}</CurrentUserProvider>
+      </Show>
     </main>
-  )
+  );
 }
