be642155b7c71c53f22279a3d87689ea603db411
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-04-07 14:45:07 +0700

    feat(seed): add initial database seeding script for User model
    Add a script to seed the database with an initial user, including password hashing and environment variable for password configuration.

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import { hashPassword } from '~/lib/hash.server';
+import prisma from '~/lib/db';
+import { Prisma } from '~/generated/prisma/client';
+
+const seedUser = async () => {
+  const INITIAL_USER_PASSWORD = process.env.INITIAL_USER_PASSWORD;
+  if (!INITIAL_USER_PASSWORD) {
+    throw new Error('INITIAL_USER_PASSWORD is not defined');
+  }
+  const hashedPassword = await hashPassword(INITIAL_USER_PASSWORD);
+  const userData: Prisma.UserCreateInput = {
+    name: 'Dimas',
+    email: 'alfi.dim@gramedia.com',
+    nik: '123456',
+    password: hashedPassword,
+  };
+
+  console.log('Seeding user: ', userData.email);
+  const user = await prisma.user.create({ data: userData });
+  console.log('User created: ', user.email);
+};
+
+async function main() {
+  await seedUser();
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
