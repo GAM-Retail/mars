a44c48bdb86edc8976fe1064325c31283219bcae
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-04-07 14:49:44 +0700

    style(routes): fix import spacing and update username to name display

--- src/routes/(protected)/index.tsx
+++ src/routes/(protected)/index.tsx
@@ -1,12 +1,11 @@
-import { logout } from "~/lib";
-import {useCurrentUser} from "~/hooks/useCurrentUser";
-
+import { logout } from '~/lib';
+import { useCurrentUser } from '~/hooks/useCurrentUser';
 
 export default function Home() {
-  const userContext = useCurrentUser()
+  const userContext = useCurrentUser();
   return (
     <div class="w-full space-y-2">
-      <h2 class="font-bold text-3xl text-violet-500">Hello {userContext?.currentUser?.username}</h2>
+      <h2 class="font-bold text-3xl text-primary">Hello {userContext?.currentUser?.name}</h2>
       <h3 class="font-bold text-xl">Message board</h3>
       <form action={logout} method="post">
         <button name="logout" type="submit">
