c68d96a8f375d96f85beef234659b6f06f071128
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-04-17 07:38:11 +0700

    feat(facility): add CRUD operations for facility entity in repository

---
+++ src/server/repository/facility.server.ts
@@ -0,0 +1,41 @@
+import db from '~/lib/db';
+export const add = async (facility: { name: string; createdBy: string; description?: string }) => {
+  return db.facility.create({
+    data: {
+      name: facility.name,
+      description: facility?.description ?? '',
+      createdBy: facility.createdBy,
+    },
+  });
+};
+
+export const edit = async (facility: { id: string; name: string; description?: string }) => {
+  return db.facility.update({
+    data: {
+      name: facility.name,
+      description: facility?.description ?? '',
+    },
+    where: {
+      id: facility.id,
+    },
+  });
+};
+
+export const getAll = async () => {
+  return db.facility.findMany({
+    orderBy: { createdAt: 'desc' },
+  });
+};
+
+export const getById = async (id: string) => {
+  return db.facility.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      createdByUser: true,
+    },
+  });
+};
+
+export const deleteById = async (id: string) => db.facility.delete({ where: { id } });
