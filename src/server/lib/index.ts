94f1ba6a7b9f38a709a1eae6f7756d57147f1cc3
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-04-23 15:50:00 +0700

    feat(server): add validateSessionWithRole to check user role and auth

--- src/server/lib/index.ts
+++ src/server/lib/index.ts
@@ -1,10 +1,29 @@
 import { getSession } from '~/lib/auth.server';
+import db from '~/lib/db';
+import { UserRole } from '~/types';
+import { ForbiddenError } from '~/lib/error';
 
 export const validateSession = async (): Promise<string> => {
   const session = await getSession();
   const userId = session.data.userId;
 
   if (!userId) throw new Error('Unauthorized');
+
+  return userId;
+};
+
+export const validateSessionWithRole = async (requiredRole: UserRole): Promise<string> => {
+  const session = await getSession();
+  const userId = session.data.userId;
+
+  if (!userId) throw new ForbiddenError('You must be logged in to access this resource');
+
+  const user = await db.user.findUnique({ where: { id: userId } });
+  if (!user) throw new ForbiddenError('User not found');
+
+  if (user.role !== requiredRole) {
+    throw new ForbiddenError();
+  }
 
   return userId;
 };
