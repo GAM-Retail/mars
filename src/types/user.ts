ea4da6f59a65281a4db85028fb615accd533d6bf
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-04-24 10:20:05 +0700

    feat(types): add createdAt and updatedAt to CurrentUser type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,12 @@
 import { UserRole } from '~/generated/prisma/enums';
 
-export type CurrentUser = { id: string; name: string; email: string; nik: string; role: UserRole };
+export type CurrentUser = {
+  id: string;
+  name: string;
+  email: string;
+  nik: string;
+  role: UserRole;
+  createdAt: Date;
+  updatedAt: Date;
+};
 export { UserRole } from '~/generated/prisma/enums';
