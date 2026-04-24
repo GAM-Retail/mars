e511d82ceb11eea76559631e1b6284bf37d05a20
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-04-24 07:41:47 +0700

    style(user): fix formatting and update grid breakpoint from md to sm

--- src/routes/(protected)/user/[id]/components/UserDetail.tsx
+++ src/routes/(protected)/user/[id]/components/UserDetail.tsx
@@ -54,7 +54,11 @@ export default function UserDetail(props: Readonly<Props>) {
               <Cog class="h-6 w-6" />
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuItem as={A} href={`/user/${props.user.id}/edit`} onSelect={() => navigate(`/user/${props.user.id}/edit`)}>
+              <DropdownMenuItem
+                as={A}
+                href={`/user/${props.user.id}/edit`}
+                onSelect={() => navigate(`/user/${props.user.id}/edit`)}
+              >
                 Edit
               </DropdownMenuItem>
               <DropdownMenuItem
@@ -89,7 +93,7 @@ export default function UserDetail(props: Readonly<Props>) {
         </div>
       </div>
 
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
         <div class="space-y-4">
           <div>
             <p class="text-xs text-muted-foreground mb-1">NIK</p>
