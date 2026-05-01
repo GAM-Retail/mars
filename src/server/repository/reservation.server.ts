df71082db89543bc70106aee67f747fc2032a766
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-05-01 23:43:39 +0700

    feat(repository): add reservation server repository methods

---
+++ src/server/repository/reservation.server.ts
@@ -0,0 +1,76 @@
+import db from '~/lib/db';
+
+export const getAllReservationsByPersonInCharge = async (userId: string, roomIds?: string[]) => {
+  if (roomIds?.length === 0) {
+    return [];
+  }
+
+  return db.roomReservation.findMany({
+    where: {
+      roomId: { in: roomIds },
+    },
+    include: {
+      room: true,
+      reservedBy: { select: { id: true, name: true, email: true } },
+      organizer: true,
+    },
+    orderBy: { startTime: 'desc' },
+  });
+};
+
+export const getReservationById = async (id: string) => {
+  return db.roomReservation.findUnique({
+    where: { id },
+    include: {
+      room: true,
+      reservedBy: { select: { id: true, name: true, email: true } },
+      organizer: true,
+    },
+  });
+};
+
+export const createReservation = async (data: {
+  roomId: string;
+  reservedById: string;
+  organizerId: string;
+  startTime: Date;
+  endTime: Date;
+  agenda?: string;
+}) => {
+  return db.roomReservation.create({
+    data: {
+      roomId: data.roomId,
+      reservedById: data.reservedById,
+      organizerId: data.organizerId,
+      startTime: data.startTime,
+      endTime: data.endTime,
+      agenda: data.agenda,
+    },
+  });
+};
+
+export const updateReservation = async (data: {
+  id: string;
+  roomId: string;
+  organizerId: string;
+  startTime: Date;
+  endTime: Date;
+  agenda?: string;
+}) => {
+  return db.roomReservation.update({
+    data: {
+      roomId: data.roomId,
+      organizerId: data.organizerId,
+      startTime: data.startTime,
+      endTime: data.endTime,
+      agenda: data.agenda,
+    },
+    where: { id: data.id },
+  });
+};
+
+export const deleteReservation = async (id: string) => {
+  return db.roomReservation.delete({
+    where: { id },
+  });
+};
