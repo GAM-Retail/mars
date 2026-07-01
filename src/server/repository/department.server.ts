91060a4d1564853618d51d425a4b53602a67f6dd
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-07-01 11:20:50 +0700

    feat(department): add CRUD operations and controller actions

---
+++ src/server/repository/department.server.ts
@@ -0,0 +1,34 @@
+import db from '~/lib/db';
+
+export const add = async (department: { name: string }) => {
+  return db.department.create({
+    data: {
+      name: department.name,
+    },
+  });
+};
+
+export const edit = async (department: { id: string; name: string }) => {
+  return db.department.update({
+    data: {
+      name: department.name,
+    },
+    where: {
+      id: department.id,
+    },
+  });
+};
+
+export const getAll = async () => {
+  return db.department.findMany({
+    orderBy: { createdAt: 'desc' },
+  });
+};
+
+export const getById = async (id: string) => {
+  return db.department.findUnique({
+    where: { id },
+  });
+};
+
+export const deleteById = async (id: string) => db.department.delete({ where: { id } });
