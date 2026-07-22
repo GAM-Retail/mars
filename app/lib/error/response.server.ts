2908a2e60e116a4d3dc8b4b3bfe5cd511c170689
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-07-22 15:05:45 +0700

    feat(error): enhance prisma error handling with user-friendly messages

--- app/lib/error/response.server.ts
+++ app/lib/error/response.server.ts
@@ -1,6 +1,25 @@
-export function catchResult(err: unknown) {
-  return {
-    success: false,
-    message: err instanceof Error ? err.message : 'An unexpected error occurred',
-  };
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+import { dataWithToast } from '~/lib/utils.server';
+
+export function catchResult(request: Request, err: unknown) {
+  let message = err instanceof Error ? err.message : 'An unexpected error occurred';
+  if (err instanceof PrismaClientKnownRequestError) {
+    if (err.code === 'P2000') {
+      message = 'Input value is too long';
+    }
+    if (err.code === 'P2002') {
+      message = `Duplicate value violates unique constraint`;
+      if (err.message.includes('email')) {
+        message = 'The provided email is already in use. Please choose a different email.';
+      }
+      if (err.message.includes('nik')) {
+        message = 'NIK is not valid, please choose a different NIK';
+      }
+    }
+  }
+  return dataWithToast(request, null, {
+    type: 'error',
+    title: 'Oops! Something went wrong',
+    description: message,
+  });
 }
